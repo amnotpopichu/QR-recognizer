e4a728653e64e1ed0037b2d39cb4e4962dcbbe18
amnotpopichu <leohsia2010@gmail.com>
2024-02-10 22:23:27 -0800

    comments

--- QR.py
+++ QR.py
@@ -1,5 +1,9 @@
 import numpy as np
 import cv2
+#end goal
+#get robot to move by itself and align nicly
+#then work with the other 3 qr codes (ftc)
+
 
 camera_id = 0
 delay = 1
@@ -23,6 +27,8 @@ while True:
                 if any(values[i][0] < 200 or values[i][0] > 500 or values[i][1] < 200 or values[i][1] > 500 for i in range(4)):
                     frame = cv2.putText(frame, "outside", (200, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
                 elif any(300 < values[i][0] < 400 and 300 < values[i][1] < 400 for i in range(4)):
+                    #move closer currently only checks if its inside that small box, so if its super far away but aligned perfectly, it will think its inside
+                    #to be fixed
                     frame = cv2.putText(frame, "move closer", (200, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
                 else:
                     frame = cv2.putText(frame, "inside", (200, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
