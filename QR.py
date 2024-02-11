5b48db352ae92c4f425983a6c4ad18382420b2bd
amnotpopichu <leohsia2010@gmail.com>
2024-02-10 22:20:27 -0800

    Update QR.py
    tood:
    fix
    #move closer currently only checks if its inside that small box, so if its super far away but aligned perfectly, it will think its inside
                        #to be fixed

--- QR.py
+++ QR.py
@@ -1,3 +1,4 @@
+import numpy as np
 import cv2
 
 camera_id = 0
@@ -6,28 +7,31 @@ window_name = 'OpenCV QR Code'
 
 qcd = cv2.QRCodeDetector()
 cap = cv2.VideoCapture(camera_id)
+
 while True:
-    
     ret, frame = cap.read()
 
     if ret:
         ret_qr, decoded_info, points, _ = qcd.detectAndDecodeMulti(frame)
+        
         if ret_qr:
             for s, p in zip(decoded_info, points):
                 color = (0, 255, 0)
-                #0,0 gives x of verticy 1
-                #remeber your ranges and it starrts at 0 lol
                 values = p.astype(int)
-                if all(200 < values[i][j] < 500 for i in range(4) for j in range(2)):
-                    frame = cv2.putText(frame, "inside", (200, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-                else:
-                    frame = cv2.putText(frame, "outside", (200, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
-                
+                # Updated condition order
+                if any(values[i][0] < 200 or values[i][0] > 500 or values[i][1] < 200 or values[i][1] > 500 for i in range(4)):
+                    frame = cv2.putText(frame, "outside", (200, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+                elif any(300 < values[i][0] < 400 and 300 < values[i][1] < 400 for i in range(4)):
+                    frame = cv2.putText(frame, "move closer", (200, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+                else:
+                    frame = cv2.putText(frame, "inside", (200, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+
 
                 frame = cv2.polylines(frame, [p.astype(int)], True, color, 8)
-        frame = cv2.rectangle(frame,(200,200),(500,500),(0,0,255),8)
-        cv2.imshow(window_name, frame)
+            
+            frame = cv2.rectangle(frame, (200, 200), (500, 500), (0, 0, 255), 8)
+            cv2.imshow(window_name, frame)
 
     if cv2.waitKey(delay) & 0xFF == ord('q'):
         break
