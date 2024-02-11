e56f497846ef6c7e98ae2453e67fd63eb3667f49
amnotpopichu <leohsia2010@gmail.com>
2024-02-10 21:56:39 -0800

    simplified

--- QR.py
+++ QR.py
@@ -17,17 +17,12 @@ while True:
                 color = (0, 255, 0)
                 #0,0 gives x of verticy 1
                 #remeber your ranges and it starrts at 0 lol
-                #https://note.nkmk.me/en/python-opencv-qrcode/
-                #if need to identify specific qr code for ftc check here and try to identifiy
-                if 500>p.astype(int)[0][0]>200 and 500>p.astype(int)[0][1]>200 and 500>p.astype(int)[1][0]>200 and 500>p.astype(int)[1][1]>200 and 500>p.astype(int)[2][0]>200 and 500>p.astype(int)[2][1]>200 and 500>p.astype(int)[3][0]>200 and 500>p.astype(int)[3][1]>200:
-                    frame = cv2.putText(frame, "inside", (200,100), cv2.FONT_HERSHEY_SIMPLEX,  
-                            1, (0,0,255), 2, cv2.LINE_AA) 
-                elif 400>p.astype(int)[0][0]>300 and 400>p.astype(int)[0][1]>300 and 400>p.astype(int)[1][0]>300 and 400>p.astype(int)[1][1]>300 and 400>p.astype(int)[2][0]>300 and 400>p.astype(int)[2][1]>300 and 400>p.astype(int)[3][0]>300 and 400>p.astype(int)[3][1]>300:
-                    frame = cv2.putText(frame, "too far away", (200,100), cv2.FONT_HERSHEY_SIMPLEX,  
-                            1, (0,0,255), 2, cv2.LINE_AA) 
+                values = p.astype(int)
+                if all(200 < values[i][j] < 500 for i in range(4) for j in range(2)):
+                    frame = cv2.putText(frame, "inside", (200, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
                 else:
-                    frame = cv2.putText(frame, "outside", (200,100), cv2.FONT_HERSHEY_SIMPLEX,  
-                            1, (0,0,255), 2, cv2.LINE_AA) 
+                    frame = cv2.putText(frame, "outside", (200, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+
                 
 
                 frame = cv2.polylines(frame, [p.astype(int)], True, color, 8)
